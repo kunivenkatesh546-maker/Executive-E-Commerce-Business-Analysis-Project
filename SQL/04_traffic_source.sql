ab6a57ef71612cc4f7b3ceda466d8b909d21c313
Venkatesh N <kunivenkatesh546@gmail.com>
2026-09-17 19:48:49 +0530

    Delete SQL directory

--- SQL/04_traffic_source.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    traffic_source.source AS source,
-    COUNT(DISTINCT user_pseudo_id) AS unique_users
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-WHERE traffic_source.source IS NOT NULL
-GROUP BY source
-ORDER BY unique_users DESC;
--- SQL/07_top_purchase_analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  items.item_name,
-  COUNT(*) AS purchased
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
-UNNEST(items) AS items
-WHERE event_name = 'purchase'
-GROUP BY items.item_name
-ORDER BY purchased DESC
-LIMIT 10;
