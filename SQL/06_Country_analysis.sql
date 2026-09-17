3a53e9c2c7eee2a45b7edbacc671bd9aff86581c
Venkatesh N <kunivenkatesh546@gmail.com>
2026-09-17 19:34:33 +0530

    Delete SQL directory

--- SQL/06_Country_analysis.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  geo.country AS location,
-  COUNT(DISTINCT user_pseudo_id) AS users
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-GROUP BY location
-ORDER BY users DESC;
